6cca6d6a323e238ef826ff553c024c13f63a2749
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-10-27 17:24:12 +0100

    Add two numpy methods

--- estimate.py
+++ estimate.py
@@ -1,5 +1,24 @@
 import sys
+import numpy
 import random
+
+def throw_darts_numpy_random(amount):
+    # dart throw: numpy random uniform
+    hits = 0
+    for i in range(int(amount)):
+        x = numpy.random.uniform(0, 1)
+        y = numpy.random.uniform(0, 1)
+        hits += x**2 + y**2 <= 1
+    return hits
+
+def throw_darts_numpy_random_sample(amount):
+    # dart throw: numpy random uniform array
+    hits = 0
+    xs = numpy.random.random(amount)
+    ys = numpy.random.random(amount)
+    for i in range(int(amount)):
+        hits += xs[i]**2 + ys[i]**2 <= 1
+    return hits
 
 def throw_darts_pure_python(amount):
     # dart throw: pure python
@@ -11,6 +30,8 @@ def throw_darts_pure_python(amount):
     return hits
 
 methods = (
+        'numpy_random',
+        'numpy_random_sample',
         'pure_python',
         )
 
