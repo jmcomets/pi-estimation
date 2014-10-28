a779c7b3f9a761d53ce89a6f088e08ef6525cb53
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-10-28 12:12:43 +0100

    Add method handling to main program

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import estimate
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('iterations', metavar='iterations', type=int,
         help='number of iterations to run')
+parser.add_argument('-m', '--method', type=str,
+        choices=estimate.methods, default=estimate.methods[0],
+        help='method of computation (optimization)')
 args = parser.parse_args()
 
 if __name__ == '__main__':
