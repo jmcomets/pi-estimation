17c99bd5942637bf9a9a46c164c5c91ad39a45a5
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-10-29 11:24:47 +0100

    Finalize benchmarking scripts

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,6 +1,5 @@
 import sys; sys.path.append('..')
 import time
-import math
 import numpy
 import estimate
 
@@ -10,12 +9,12 @@ max_iters_pow = 5
 precision = 50
 nb_tries = 10
 
-print('nb_iters,time,error')
+print('nb_iters,time,estimation')
 for nb_iters in numpy.logspace(min_iters_pow, max_iters_pow, precision):
     nb_iters = int(nb_iters)
     for _ in range(nb_tries):
         init_time = time.time()
         estimated_pi = estimate.pi(nb_iters, 'pure_python')
         dt = time.time() - init_time
-        print('%s,%s,%s' % (nb_iters, dt, abs(math.pi - estimated_pi)))
+        print('%s,%s,%s' % (nb_iters, dt, estimated_pi))
         sys.stdout.flush()
--- scripts/plot.py
+++ scripts/plot.py
@@ -1,6 +1,8 @@
 import sys
 import csv
-import statistics
+import math
+from statistics import mean as avg
+from collections import OrderedDict
 import matplotlib.pyplot as plt
 
 if len(sys.argv) < 2:
@@ -8,28 +10,41 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 # read data
-benchs = {}
-print('reading data')
+benchs = OrderedDict()
 with open(sys.argv[1], 'r') as fp: # read input file
     reader = csv.reader(fp)
     next(reader) # ignore header
     for line in reader:
         try:
-            nb_iters, time, error = line
-            nb_iters, time, error = int(nb_iters), float(time), float(error)
-            benchs.setdefault(nb_iters, [[], []])
-            benchs[nb_iters][0].append(time)
-            benchs[nb_iters][1].append(error)
+            nb_iters, time, estimation = line
+            nb_iters, time, estimation = int(nb_iters), float(time), float(estimation)
+            bench = benchs.setdefault(nb_iters, {
+                'estimations': [],
+                'times': []
+                })
+            bench['estimations'].append(estimation)
+            bench['times'].append(time)
         except ValueError:
             print('ignored line %s, bad format' % line)
 
 # plot data
-print('plotting data')
-xs, time_ys, error_ys = [], [], []
-for nb_iters in benchs:
-    xs.append(nb_iters)
-    times, errors = benchs[nb_iters]
-    time_ys.append(statistics.mean(times))
-    error_ys.append(statistics.mean(errors))
-plt.plot(xs, time_ys); plt.show()
-plt.plot(xs, error_ys); plt.show()
+m, n = 1, 1
+xs = list(benchs.keys())
+plt.xlabel('Number of iterations')
+
+# all estimations
+sp1 = plt.subplot(m, n, 1)
+sp1.set_xscale('log')
+for x, b in benchs.items():
+    sp1.scatter([x] * len(b['estimations']), b['estimations'], c='r')
+sp1.set_ylabel('All estimations', color='r')
+
+# math.pi horizontal line
+sp1.axhline(y=math.pi, c='y')
+
+# average time
+sp2 = sp1.twinx()
+sp2.plot(xs, [avg(b['times']) for b in benchs.values()], c='b')
+sp2.set_ylabel('Average runtime (s)', color='b')
+
+plt.show()
